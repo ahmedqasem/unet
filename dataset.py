5710b7c0d16d9cb63a955f7d6dd6497f213b5dac
ahmed <ahmedqasem2@outlook.com>
2023-02-06 14:46:51 -0600

    converted dataset to jpg

--- dataset.py
+++ dataset.py
@@ -46,44 +46,53 @@ class HecktorDataset(Dataset):
         self.images = os.listdir(image_dir)
 
     def __len__(self):
+        try:
+            assert len(self.images)//2 == len(self.images)/2
+            return len(self.images)//2
+        except Exception as e:
+            print(e)
+            return 0
         
-        # for each image folder get the slices for CT image
-        slices = 0
-        for image in self.images:
-            CT = os.path.join(self.image_dir, f'{image}/{image}__CT.nii.gz')
-            slices += nib.load(CT).get_fdata().shape[2]
-        return slices
 
     def __getitem__(self, index):
         # set the path to load images from
         img_path = os.path.join(self.image_dir, self.images[index])
         # print(f'loading images from {img_path}')
-        folder_name = img_path.split('\\')[-1]
-        # print(f'folder name is {folder_name}')
+        slice_name = img_path.split('\\')[-1].split('__')[0]
+        # print(f'folder name is {slice_name}')
 
-        CT_img_path = os.path.join(img_path, f'{folder_name}__CT.nii.gz')
+        CT_img_path = os.path.join(self.image_dir, f'{slice_name}__CT.png')
         # print(f'CT image file name is {CT_img_path}')
         
-        PET_img_path = CT_img_path.replace('__CT.nii.gz', '__PT.nii.gz')
+        PET_img_path = CT_img_path.replace('__CT.png', '__PT.png')
         # print(f'loading PET from {PET_img_path}')
 
-        mask_path = os.path.join(self.mask_dir, f'{folder_name}.nii.gz')
+        mask_path = os.path.join(self.mask_dir, f'{slice_name}.png')
         # print(f'loading mask from {mask_path}')
         # load images and mask
-        CT_image = nib.load(CT_img_path).get_fdata()
-        CT_image_norm = (CT_image-np.min(CT_image))/(np.max(CT_image)-np.min(CT_image))
-        PET_image = nib.load(PET_img_path).get_fdata()
-        # normalize pet image
-        PET_image_norm = (PET_image-np.min(PET_image))/(np.max(PET_image)-np.min(PET_image))
-        print(np.max(PET_image_norm))
-        mask = nib.load(mask_path).get_fdata()
-
-        # resize PET image
-        pet_r = cv2.resize(PET_image_norm, (CT_image_norm.shape[0], CT_image_norm.shape[1]) )
-        # convert PET CT to 2 channel image
-        image = np.moveaxis(np.concatenate((np.expand_dims(CT_image, axis=0), np.expand_dims(pet_r, axis=0)), axis=0), 0, -2)
-        # change mask values from 255 t0 0-1
+        CT_image = np.array(Image.open(CT_img_path).convert('L'))
+        # print('ct shape is: ', CT_image.shape)
+        PET_image = np.array(Image.open(PET_img_path).convert('L'))
+        # print('pett shape is: ', PET_image.shape)
+        mask = np.array(Image.open(mask_path).convert('L'))
+        # print('mask shape is: ', mask.shape)
+        
+        # normalize
+        # print(f'ct values {np.min(CT_image)}, {np.max(CT_image)}')
+        # print(f'PET values {np.min(PET_image)}, {np.max(PET_image)}')
+        # print(f'mask values {np.min(mask)}, {np.max(mask)}')
+        CT_image = CT_image/255
+        PET_image = PET_image/255
         mask[mask==255.0] = 1
+
+        # print(f'ct values {np.min(CT_image)}, {np.max(CT_image)}')
+        # print(f'PET values {np.min(PET_image)}, {np.max(PET_image)}')
+        # print(f'mask values {np.min(mask)}, {np.max(mask)}')
+
+
+        # convert PET CT to 2 channel image
+        image = np.moveaxis(np.concatenate((np.expand_dims(CT_image, axis=0), np.expand_dims(PET_image, axis=0)), axis=0), 0, -1)
+        # print(image.shape)
 
         # perform data augmentation using the albemntations library 
         if self.transform is not None:
@@ -129,20 +138,45 @@ def display_image(image, mask, idx=35):
     plt.show()
 
 
+def display_image2(image, mask, idx=35):
+    n=35
+    fig, ax = plt.subplots(ncols=2, nrows=4, figsize=(15,30))
+    ax[0][0].imshow(image[:,:,0], cmap='gray')
+    ax[0][0].set_title('CT Image')
+    ax[0][1].imshow(image[:,:,1], cmap='gray')
+    ax[0][1].set_title('PET image')
+    ax[1][0].imshow(mask, cmap='gray')
+    ax[1][0].set_title('Mask Image')
+    # # contour
+    ax[1][1].imshow(image[:,:,0], cmap='gray')
+    ax[1][1].contour(mask)
+    ax[2][0].imshow(image[:,:,1], cmap='gray')
+    ax[2][0].contour(mask)
+    ax[2][1].imshow(image[:,:,0], cmap='gray')
+    ax[2][1].contour(image[:,:,1])
+    # # all together 
+    ax[3][0].imshow(image[:,:,0], cmap='gray')
+    ax[3][0].contour(image[:,:,1])
+    ax[3][0].contour(mask, colors='red')
+    plt.show()
+
+
 def test_hecktor():
-    img = HecktorDataset(   image_dir='./data/train_images',
-                            mask_dir= './data/train_masks')
+
+    image_dir ='E:/Datasets/monte_carlo_segmentation/hecktor2022_training/jpg/train_images'
+    mask_dir = 'E:/Datasets/monte_carlo_segmentation/hecktor2022_training/jpg/train_masks'
+    img = HecktorDataset(image_dir=image_dir, mask_dir=mask_dir)
     print(f'found {img.__len__()} PET/CT image pairs')
     
-    image, mask = img.__getitem__(1)
-    # print(f'found image: {image.shape}')
+    image, mask = img.__getitem__(61)
+    print(f'found image: {image.shape}')
     # print(f'moved axis {np.moveaxis(image, 2, 0).shape}')
     # print(np.moveaxis(np.moveaxis(image, 2, 0), 3, 1).shape)
     # print(f'found mask: {np.moveaxis(mask, 2, 0)[0].shape}')
 
     # view sample image and GT
-    idx = random.randint(0, image.shape[3])
-    display_image(image, mask, idx)
+    idx = random.randint(0, img.__len__())
+    display_image2(image, mask, idx)
 
 
 if __name__ == "__main__":
